f7d674e4e4ab745278081cf1a442dfc2e667be23
Micheal Bailey <mbaileyfuturist@gmail.com>
2022-03-21 21:51:00 -0400

    Solution

--- src/main/java/com/revature/Solution.java
+++ src/main/java/com/revature/Solution.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public class Solution {
 
 	/**
@@ -10,8 +12,33 @@ public class Solution {
 	
 	public static int[] solution(int[] data, int n) {
 		
+		if(data.length > 100) {
+			System.exit(0);
+		}else {
+			
+			int numberOfOccurences = 0;		
+			int currentNumber = 0;
+			
+			for(int index = 0;index < data.length;index++) {
+				currentNumber = data[index];
+				for(int index2 = 1; index2 < data.length;index2++) {
+					if(currentNumber == data[index2]) {
+						numberOfOccurences++;
+						if(numberOfOccurences > n) {
+							for(int index3 = 0;index3 < data.length; index3++) {
+								if(data[index3] == currentNumber) {
+									data = ArrayUtils.remove(data, index3);
+								}
+							}
+						}
+					}
+				}
+				numberOfOccurences = 0;
+			}
+		}
 		
-		return null;
+		
+		return data;
 	}
 
 }
